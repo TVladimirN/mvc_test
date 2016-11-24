79bf9ddae81b4f7b6a074c5b5141a4b3a6fd4633
Tvladimir <lbo4ukl@gmail.com>
2016-11-24 22:24:33 +0400

    test

--- src/main/java/ru/server/configs/WebMvcConfig.java
+++ src/main/java/ru/server/configs/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package ru.server.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -12,6 +16,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("images/**").addResourceLocations("images/");
+    }
+
+    // стандартный resolver представлений, для небольших приложений избыточен, надобности нет
+    // использован конечный класс InternalResourceViewResolver, но разницы в работе с UrlBasedViewResolver не замечено
+    // использование JstlView позволяет делать JSTL-инъекции в динамические страницы или фрагменты страниц
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
     }
 
 //    @Override
